ab6ac4a2e6d1c42b2e299d44fdb0b871dae9395d
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-09-14 15:08:57 -0700

    added functional mobile menu

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // make functional hamburger menu for mobile
 
-// add event listener (click or mouseOver)
+const mobileIcon = document.querySelector('.fa-bars');
+const mobileMenu = document.querySelector('nav ul')
 
+
+// add event listener (click or mouseOver)
 // display nav items on event
+mobileIcon.addEventListener('mouseover', () => {
+    mobileMenu.className = "open";
+});
+
+mobileIcon.addEventListener('mouseout', () => {
+    mobileMenu.className = "";
+})
